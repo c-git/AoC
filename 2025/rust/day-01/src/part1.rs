0b1b0be74c788633209345f49f3203bfbe2865e4
One <43485962+c-git@users.noreply.github.com>
2025-12-02 11:56:18 -0500

    Solve first part

--- 2025/rust/day-01/src/part1.rs
+++ 2025/rust/day-01/src/part1.rs
@@ -1,6 +1,28 @@
+use miette::{Context, IntoDiagnostic};
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut result = 0;
+    let mut curr_position = 50;
+    for line in input.lines() {
+        let sign = if line.starts_with("L") { -1 } else { 1 };
+        let clicks: i16 = line[1..]
+            .parse()
+            .into_diagnostic()
+            .wrap_err("failed parsing of number clicks")?;
+        curr_position += sign * clicks;
+        if curr_position < 0 {
+            curr_position += 100;
+        }
+        if curr_position > 99 {
+            curr_position -= 100;
+        }
+        if curr_position == 0 {
+            result += 1;
+        }
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +31,18 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
+        assert_eq!(process(input)?, "3");
         Ok(())
     }
 }
