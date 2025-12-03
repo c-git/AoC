4227aa8ca1f4e9ecb37234b3da392fa78b2e6e15
One <43485962+c-git@users.noreply.github.com>
2025-12-03 13:34:08 -0500

    First attempt

--- 2025/rust/day-03/src/part1.rs
+++ 2025/rust/day-03/src/part1.rs
@@ -1,6 +1,36 @@
+use miette::Context;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day-03 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut result = 0;
+    for line in input.lines() {
+        let mut first_num = None;
+        let mut second_num = None;
+        for c in line.chars() {
+            let curr_num = c.to_digit(10).wrap_err("conversion failed")?;
+            match (first_num, second_num) {
+                (None, None) => first_num = Some(curr_num),
+                (None, Some(_)) => unreachable!("always fill first number first"),
+                (Some(_), None) => {
+                    second_num = Some(curr_num);
+                }
+                (Some(first), Some(second)) => {
+                    if first < second {
+                        first_num = second_num;
+                        second_num = Some(curr_num)
+                    } else if second < curr_num {
+                        second_num = Some(curr_num)
+                    }
+                }
+            }
+        }
+        if let (Some(first), Some(second)) = (first_num, second_num) {
+            result += first * 10 + second;
+        } else {
+            unreachable!("we should always get at least two numbers");
+        }
+    }
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,8 +39,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111
+";
+        assert_eq!(process(input)?, "357");
         Ok(())
     }
 }
