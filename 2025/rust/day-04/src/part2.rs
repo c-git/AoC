34e42a8df13c761d34e12bfe25a4b49a1f7b70b7
One <43485962+c-git@users.noreply.github.com>
2025-12-04 10:17:40 -0500

    Create Day 4

---
+++ 2025/rust/day-04/src/part2.rs
@@ -0,0 +1,17 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day-04 - part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore = "not on second part yet"]
+    fn test_process() -> miette::Result<()> {
+        let input = "";
+        assert_eq!(process(input)?, "");
+        Ok(())
+    }
+}
