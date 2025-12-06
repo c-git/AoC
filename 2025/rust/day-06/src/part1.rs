d60325c71c6cb5db92dc60d1e34672783a7c2f32
One <43485962+c-git@users.noreply.github.com>
2025-12-06 17:48:03 -0500

    Day 6 - Part 1

--- 2025/rust/day-06/src/part1.rs
+++ 2025/rust/day-06/src/part1.rs
@@ -1,6 +1,65 @@
+use miette::{Context, IntoDiagnostic, bail};
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day-06 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut result = 0u64;
+    let mut rows = vec![];
+    for line in input.lines() {
+        rows.push(line.split_whitespace().collect::<Vec<_>>());
+    }
+    let row_count = rows.len();
+    let col_count = rows[0].len();
+    debug_assert!(
+        rows.iter().all(|x| col_count == x.len()),
+        "all rows should have same number of columns"
+    );
+    for col in 0..col_count {
+        let operation = Operation::try_from(rows[row_count - 1][col])?;
+        let mut sub_result = operation.default_accumulator();
+        for row in rows.iter().take(row_count - 1) {
+            let value = row[col]
+                .parse()
+                .into_diagnostic()
+                .wrap_err("failed to convert to number")?;
+            sub_result = operation.perform(sub_result, value);
+        }
+        result += sub_result;
+    }
+    Ok(result.to_string())
+}
+
+enum Operation {
+    Add,
+    Multiply,
+}
+
+impl Operation {
+    #[must_use]
+    fn perform(&self, x: u64, y: u64) -> u64 {
+        match self {
+            Operation::Add => x + y,
+            Operation::Multiply => x * y,
+        }
+    }
+
+    fn default_accumulator(&self) -> u64 {
+        match self {
+            Operation::Add => 0,
+            Operation::Multiply => 1,
+        }
+    }
+}
+
+impl TryFrom<&str> for Operation {
+    type Error = miette::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(match value {
+            "+" => Self::Add,
+            "*" => Self::Multiply,
+            other => bail!("unexpected operation found: {other:?}"),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -9,8 +68,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+";
+        assert_eq!(process(input)?, "4277556");
         Ok(())
     }
 }
