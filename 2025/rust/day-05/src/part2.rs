b8a159a2e43817a5149d03317eec4d3356f2148a
One <43485962+c-git@users.noreply.github.com>
2025-12-05 20:34:18 -0500

    Day 5 - Part 2

--- 2025/rust/day-05/src/part2.rs
+++ 2025/rust/day-05/src/part2.rs
@@ -1,6 +1,68 @@
+use miette::{Context, IntoDiagnostic};
+use std::collections::BTreeMap;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day-05 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut result = 0;
+    // Start end pairs
+    let mut fresh_ranges: BTreeMap<u64, u64> = BTreeMap::new();
+
+    let mut lines = input.lines();
+
+    // Read ranges
+    loop {
+        let line = lines
+            .next()
+            .wrap_err("should always have ranges at the top and a empty line in valid input")?;
+        if line.trim().is_empty() {
+            // Remaining lines are queries
+            break;
+        }
+        let (mut start, mut end) = parse_range(line)?;
+        let mut remove_list = vec![];
+        for (&existing_start, &existing_end) in fresh_ranges.range(..=end).rev() {
+            if start <= existing_end {
+                remove_list.push(existing_start);
+            } else {
+                break;
+            }
+        }
+        for old_start in remove_list {
+            let old_end = fresh_ranges
+                .remove(&old_start)
+                .wrap_err("we know this key must exists we just found it from the map")?;
+            start = start.min(old_start);
+            end = end.max(old_end);
+        }
+        fresh_ranges.insert(start, end);
+    }
+
+    // Add ranges
+    for (start, end) in fresh_ranges {
+        result += end - start + 1;
+    }
+
+    Ok(result.to_string())
+}
+
+fn parse_range(line: &str) -> miette::Result<(u64, u64)> {
+    let mut split = line.split("-");
+    let start = split
+        .next()
+        .wrap_err("failed to get first part of range")?
+        .parse()
+        .into_diagnostic()
+        .wrap_err("failed to parse start of range")?;
+    let end = split
+        .next()
+        .wrap_err("failed to get second part of range")?
+        .parse()
+        .into_diagnostic()
+        .wrap_err("failed to parse end of range")?;
+
+    debug_assert!(split.next().is_none(), "unexpected input found in range");
+    debug_assert!(start <= end);
+    Ok((start, end))
 }
 
 #[cfg(test)]
@@ -8,10 +70,20 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "not on second part yet"]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+        assert_eq!(process(input)?, "14");
         Ok(())
     }
 }
