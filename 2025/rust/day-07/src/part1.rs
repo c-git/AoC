48c8b153814b8e1aaffd4df2794d8f669b909a67
One <43485962+c-git@users.noreply.github.com>
2025-12-07 09:31:21 -0500

    Day 7 - Part 1

--- 2025/rust/day-07/src/part1.rs
+++ 2025/rust/day-07/src/part1.rs
@@ -1,7 +1,51 @@
+use miette::Context;
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let mut result = 0;
+    let mut lines = input.lines();
+
+    let mut beams: Vec<_> = lines
+        .next()
+        .wrap_err("first row must be present")?
+        .chars()
+        .map(|c| if c == 'S' { Cell::Beam } else { Cell::Empty })
+        .collect();
+
+    // Iterate through the lines and see where we have splitters
+    for line in lines {
+        // Get indices that have splitters
+        let splitter_positions: Vec<_> = line
+            .char_indices()
+            .filter_map(|(i, c)| if c == '^' { Some(i) } else { None })
+            .collect();
+        for splitter_position in splitter_positions {
+            if beams[splitter_position].is_beam() {
+                // A beam has hit a splitter
+                result += 1;
+                beams[splitter_position - 1] = Cell::Beam;
+                beams[splitter_position] = Cell::Empty;
+                beams[splitter_position + 1] = Cell::Beam;
+            }
+        }
+    }
+
     Ok(result.to_string())
+}
+
+enum Cell {
+    Beam,
+    Empty,
+}
+
+impl Cell {
+    /// Returns `true` if the cell is [`Beam`].
+    ///
+    /// [`Beam`]: Cell::Beam
+    #[must_use]
+    fn is_beam(&self) -> bool {
+        matches!(self, Self::Beam)
+    }
 }
 
 #[cfg(test)]
@@ -10,8 +54,24 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+";
+        assert_eq!(process(input)?, "21");
         Ok(())
     }
 }
