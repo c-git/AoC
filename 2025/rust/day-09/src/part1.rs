366e26e8e3493aba515797680b94fca2acf3c433
One <43485962+c-git@users.noreply.github.com>
2025-12-09 09:56:37 -0500

    Day 9 - Part 1

--- 2025/rust/day-09/src/part1.rs
+++ 2025/rust/day-09/src/part1.rs
@@ -1,7 +1,35 @@
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let mut result = 0;
+
+    // Collect input
+    let mut red_squares = input
+        .lines()
+        .map(|line| {
+            let pair: Vec<i64> = line
+                .split(",")
+                .map(|val| val.parse().expect("failed to parse number"))
+                .collect();
+            debug_assert_eq!(pair.len(), 2);
+            (pair[0], pair[1])
+        })
+        .collect::<Vec<_>>();
+    red_squares.sort_unstable();
+
+    // Check each pair (Room to optimize but may not be worth it)
+    for (i, first) in red_squares.iter().enumerate().take(red_squares.len() - 1) {
+        for second in red_squares.iter().skip(i + 1) {
+            result = result.max(get_area(first, second));
+        }
+    }
+
     Ok(result.to_string())
+}
+
+fn get_area(first: &(i64, i64), second: &(i64, i64)) -> u64 {
+    let length = (first.0 - second.0).abs() + 1;
+    let width = (first.1 - second.1).abs() + 1;
+    length as u64 * width as u64
 }
 
 #[cfg(test)]
@@ -10,8 +38,16 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+";
+        assert_eq!(process(input)?, "50");
         Ok(())
     }
 }
