fce4a4beefb48a486dc3fa052ae8914f166d1b8a
One <43485962+c-git@users.noreply.github.com>
2025-12-11 23:24:45 -0500

    Day 11 - Part 1

--- 2025/rust/day-11/src/part1.rs
+++ 2025/rust/day-11/src/part1.rs
@@ -1,7 +1,39 @@
+use std::collections::{BTreeMap, VecDeque};
+
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String> {
+pub fn process(input: &'static str) -> miette::Result<String> {
     let mut result = 0;
+    let graph = parse_graph(input);
+    let mut queue = VecDeque::new();
+    queue.push_back("you");
+    while let Some(next) = queue.pop_front() {
+        if next == "out" {
+            result += 1;
+            continue;
+        }
+        for &neighbour in graph.nodes.get(next).unwrap().iter() {
+            queue.push_back(neighbour);
+        }
+    }
     Ok(result.to_string())
+}
+
+fn parse_graph(input: &'static str) -> Graph {
+    let mut result = Graph::default();
+    for line in input.lines() {
+        let (node, neighbours) = line.split_once(":").unwrap();
+        let is_new = result
+            .nodes
+            .insert(node, neighbours.split_whitespace().collect())
+            .is_none();
+        debug_assert!(is_new);
+    }
+    result
+}
+
+#[derive(Debug, Default)]
+struct Graph {
+    nodes: BTreeMap<&'static str, Vec<&'static str>>,
 }
 
 #[cfg(test)]
@@ -10,8 +42,18 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+";
+        assert_eq!(process(input)?, "5");
         Ok(())
     }
 }
